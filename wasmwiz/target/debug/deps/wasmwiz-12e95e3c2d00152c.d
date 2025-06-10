4cc9e787db093a02a98184f212c862b5e31f9a12
Austin Green <austin@botzr.com>
2025-06-10 12:37:48 -0500

    init

---
+++ wasmwiz/target/debug/deps/wasmwiz-12e95e3c2d00152c.d
@@ -0,0 +1,5 @@
+/home/austingreen/Documents/botzr/projects/WasmWiz/wasmwiz/target/debug/deps/libwasmwiz-12e95e3c2d00152c.rmeta: src/main.rs
+
+/home/austingreen/Documents/botzr/projects/WasmWiz/wasmwiz/target/debug/deps/wasmwiz-12e95e3c2d00152c.d: src/main.rs
+
+src/main.rs:
---
+++ wasmwiz/target/debug/deps/wasmwiz-81529e202b323136.d
@@ -0,0 +1,5 @@
+/home/austingreen/Documents/botzr/projects/WasmWiz/wasmwiz/target/debug/deps/libwasmwiz-81529e202b323136.rmeta: src/main.rs
+
+/home/austingreen/Documents/botzr/projects/WasmWiz/wasmwiz/target/debug/deps/wasmwiz-81529e202b323136.d: src/main.rs
+
+src/main.rs:
